857e7d7d168c433d99d183c7780fbdd3927d43c5
mr-sazzad <main.sazzad@gmail.com>
2025-01-25 18:57:03 +0600

    category page updated

--- src/app/categories/[category_name]/page.tsx
+++ src/app/categories/[category_name]/page.tsx
@@ -1,27 +1,20 @@
-// import { Metadata } from "next";
-// import React from "react";
-// import CategoryProductsClient from "./CategoryProductsClient";
-
-// type Props = {
-//   params: { category_name: string };
-// };
-
-// export async function generateMetadata({ params }: Props): Promise<Metadata> {
-//   const category_name = params.category_name;
-
-//   return {
-//     title: `${category_name} Products | Alwan`,
-//     description: `Browse our collection of ${category_name} products at Alwan.`,
-//   };
-// }
-
-// export default function CategoryProductsPage({ params }: Props) {
-//   return <CategoryProductsClient />;
-// }
-
+import { Metadata } from "next";
 import React from "react";
-const Page = () => {
-  return <div>Hello From Page</div>;
+import CategoryProductsClient from "./CategoryProductsClient";
+
+type Props = {
+  params: { category_name: string };
 };
 
-export default Page;
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const category_name = params.category_name;
+
+  return {
+    title: `${category_name} Products | Alwan`,
+    description: `Browse our collection of ${category_name} products at Alwan.`,
+  };
+}
+
+export default function CategoryProductsPage({ params }: Props) {
+  return <CategoryProductsClient />;
+}
